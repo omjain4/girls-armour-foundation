199e666ad0e940c68591777b075bd16b3dd2cff2
omjain4 <119519103+omjain4@users.noreply.github.com>
2024-09-30 23:33:50 +0530

    ok

--- scripts.js
+++ scripts.js
@@ -4,3 +4,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Welcome to Girls Armour Foundation!");
 });
+function toggleNavbar() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('show');
+}
